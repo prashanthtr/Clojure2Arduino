4daa5bc7587c9b1da9d061bafcce7acfe25577da
cnmguest <cnmguest@188-75.priv23.nus.edu.sg>
2013-10-26 11:34:08 +0800

    set up emacs, clojure, git

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-git
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/clojure_git/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-git.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-git.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

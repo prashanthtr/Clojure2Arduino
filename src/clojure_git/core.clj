170e91693d9f4b786b3898ffa3203493f9acbe87
prashanthtr <cnmguest@188-75.priv23.nus.edu.sg>
2013-10-26 12:23:01 +0800

    trying to commit from emacs itself

--- src/clojure_git/core.clj
+++ src/clojure_git/core.clj
@@ -4,3 +4,13 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World here in the whole world!"))
+
+(defn printlist [list]
+
+  (set [list '(1 2 3 4)]
+
+       (println list )
+
+       )
+
+  )

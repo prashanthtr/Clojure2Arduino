2499689d2519ce1d1fdaddeac4455b213760aa10
prashanthtr <cnmguest@218-216.priv27.nus.edu.sg>
2013-10-27 01:49:03 +0800

    Set up clojure visualizations

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.keminglabs/c2 "0.2.3"]
+                 [vrepl "1.1.1"]
+                 ]
+  :min-lein-version "2.0.0"
+  :main vrepl.main
+)
--- src/clojure_git/core.clj
+++ src/clojure_git/core.clj
@@ -1,5 +1,36 @@
 (ns clojure-git.core)
 
+
+(ns bars
+  (:use [c2.core :only [unify]])
+  (:require [c2.scale :as scale]))
+
+(let [width 500, bar-height 20
+      data {"A" 1, "B" 2, "C" 4, "D" 3}
+      s (scale/linear :domain [0 (apply max (vals data))]
+                      :range [0 width])]
+
+  [:div#bars
+   (unify data (fn [[label val]]
+                 [:div {:style {:height bar-height
+                                :width (s val)
+                                :background-color "gray"}}
+                  [:span {:style {:color "white"}} label]]))])(ns bars
+  (:use [c2.core :only [unify]])
+  (:require [c2.scale :as scale]))
+
+(let [width 500, bar-height 20
+      data {"A" 1, "B" 2, "C" 4, "D" 3}
+      s (scale/linear :domain [0 (apply max (vals data))]
+                      :range [0 width])]
+
+  [:div#bars
+   (unify data (fn [[label val]]
+                 [:div {:style {:height bar-height
+                                :width (s val)
+                                :background-color "gray"}}
+                  [:span {:style {:color "white"}} label]]))])
+
 (defn foo
   "I don't do a whole lot."
   [x]
